6c4838fa45a15b3360d8a7d77aeac2d2402f0aba
Rexxi <76392112+R3XM4N@users.noreply.github.com>
2025-05-14 19:48:19 +0200

    finished code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 typedef unsigned int uint;
 
@@ -28,26 +29,13 @@ private:
     uint batchTime();
 public:
     uint roundRobin(batch &batch);
+    uint AsignBatch(batch &batch);
     uint runTime();
     void reset();
     Cpu(/* args */);
     ~Cpu() = default;
 };
 
-uint Cpu::roundRobin(batch &batch){
-    int index = 0;
-    for (size_t i = 0; i < 8; i++)
-    {
-        addTask(batch.tasks[i],index);
-        index++;
-        if (index > 2)
-        {
-            index = 0;
-        }
-    }
-    batchTime();
-    return runTime();
-}
 
 void Cpu::reset(){
     real_time = 0;
@@ -104,7 +92,7 @@ Cpu::Cpu(/* args */)
     cores[0].op_task = 'H';
     cores[1].op_task = 'M';
     cores[2].op_task = 'L';
-
+    
 }
 
 batch toBatch(char char_array[8]){
@@ -116,10 +104,83 @@ batch toBatch(char char_array[8]){
     return batch;
 }
 
+uint Cpu::roundRobin(batch &batch){
+    int index = 0;
+    for (size_t i = 0; i < 8; i++)
+    {
+        addTask(batch.tasks[i],index);
+        index++;
+        if (index > 2)
+        {
+            index = 0;
+        }
+    }
+    batchTime();
+    return runTime();
+}
+
+uint Cpu::AsignBatch(batch &batch){
+    for (size_t i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (this->cores[j].op_task == batch.tasks[i].type)
+            {
+                int indexes_lmao[3] = {j,j,j};
+                //std::cout << indexes_lmao[0] << indexes_lmao[1] << indexes_lmao[2];
+                if (++indexes_lmao[1] > 2)
+                {
+                    indexes_lmao[1] -= 3;
+                }
+                indexes_lmao[2] = indexes_lmao[1];
+                if (++indexes_lmao[2] > 2)
+                {
+                    indexes_lmao[2] -= 3;
+                }
+                //std::cout << "0-" << indexes_lmao[0] << " 1-" << indexes_lmao[1] << " 2-" << indexes_lmao[2] << "\n";
+                if (this->cores[indexes_lmao[0]].p_time + 1 <= this->cores[indexes_lmao[1]].p_time + 2 && this->cores[indexes_lmao[0]].p_time + 1 <= this->cores[indexes_lmao[2]].p_time + 2)
+                {
+                    addTask(batch.tasks[i],indexes_lmao[0]);
+                }
+                else if (this->cores[indexes_lmao[1]].p_time + 2 <= this->cores[indexes_lmao[2]].p_time + 2)
+                {
+                    addTask(batch.tasks[i],indexes_lmao[1]);
+                }
+                else{
+                    addTask(batch.tasks[i],indexes_lmao[2]);
+                }
+            }
+        }
+    }
+    batchTime();
+    return runTime();
+}
+
 int main(int argc, char** argv){
-    Cpu* cpu = new Cpu();
-    char arr[8] = {'H','M','L','H','M','L','H','M'};
-    batch batch = toBatch(arr);
-    std::cout << "Round robin: " << cpu->roundRobin(batch) << "\n";
+    Cpu* r_cpu = new Cpu();
+    Cpu* m_cpu = new Cpu();
+    std::ifstream file(argv[1], std::ios::in);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file.\n";
+        return 1;
+    }
+    int loops;
+    file >> loops;
+    loops = loops/8;
+    char data[8] = {0};
+    for (size_t i = 0; i < loops; i++)
+    {
+        for (size_t i = 0; i < 8; i++)
+        {
+            file >> data[i];
+        }
+        batch batch = toBatch(data);
+        r_cpu->roundRobin(batch);
+        m_cpu->AsignBatch(batch);
+    }
+    std::cout << "Round robin: " << r_cpu->runTime() << "\n";
+    std::cout << "Custom algorithm: " << m_cpu->runTime() << "\n";
+    std::cout << "Difference: " << std::abs((int)r_cpu->runTime() - (int)m_cpu->runTime()) << "\n";
+    
     return 0;
 }
